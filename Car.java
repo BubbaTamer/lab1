d6157850c4393fca6dec0c5cf356f26aa93262f3
Dörrich <hugo.drrich@yahoo.com>
2025-01-28 03:13:59 +0100

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import java.awt.*;
 
 public abstract class Car implements Movable {
@@ -24,15 +26,15 @@ public abstract class Car implements Movable {
     }
 
     public int getNrDoors() {
-        return nrDoors;
+        return this.nrDoors;
     }
 
     public double getEnginePower() {
-        return enginePower;
+        return this.enginePower;
     }
 
     public double getCurrentSpeed() {
-        return currentSpeed;
+        return this.currentSpeed;
     }
 
     public Color getColor() {
@@ -44,88 +46,27 @@ public abstract class Car implements Movable {
     }
 
     public void startEngine() {
-        currentSpeed = 0.1;
+        this.currentSpeed = 0.1;
     }
 
     public void stopEngine() {
-        currentSpeed = 0;
+        this.currentSpeed = 0;
     }
 
 
-
-    public static class Saab95 extends Car {
-
-        private boolean turboOn;
-
-        public Saab95(int nrDoors, Color color, double enginePower, String modelName) {
-            super(nrDoors, color, enginePower, modelName);
-            this.turboOn = false;
-        }
-
-
-        public void setTurboOn() {
-            this.turboOn = true;
-        }
-
-        public void setTurboOff() {
-            this.turboOn = false;
-        }
-
-        public double speedFactor() {
-            double turbo = 1;
-            if (turboOn) turbo = 1.3;
-            return getEnginePower() * 0.01 * turbo;
-        }
-
-        public void incrementSpeed(double amount) {
-            currentSpeed = getCurrentSpeed() + speedFactor() * amount;
-        }
-
-        public void decrementSpeed(double amount) {
-            currentSpeed = getCurrentSpeed() - speedFactor() * amount;
-        }
-
-        // TODO fix this method according to lab pm
-        public void gas(double amount) {
-            incrementSpeed(amount);
-        }
-
-        // TODO fix this method according to lab pm
-        public void brake(double amount) {
-            decrementSpeed(amount);
-        }
+    public void move() {
+        x += this.currentSpeed * Math.cos(Math.toRadians(this.direction));
+        y += this.currentSpeed * Math.sin(Math.toRadians(this.direction));
     }
 
 
-    public static class Volvo240 extends Car {
-
-        private final static double trimFactor = 1.25;
-
-        public Volvo240(int nrDoors, Color color, double enginePower, String modelName) {
-            super(nrDoors, color, enginePower, modelName);
-        }
-
-
-        public double speedFactor() {
-            return getEnginePower() * 0.01 * trimFactor;
-        }
-
-        public void incrementSpeed(double amount) {
-            currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, getEnginePower());
-        }
-
-        public void decrementSpeed(double amount) {
-            currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
-        }
-
-        // TODO fix this method according to lab pm
-        public void gas(double amount) {
-            incrementSpeed(amount);
-        }
-
-        // TODO fix this method according to lab pm
-        public void brake(double amount) {
-            decrementSpeed(amount);
-        }
+    public void turnLeft() {
+        this.direction = (this.direction - 90 + 360) % 360;
     }
+
+
+    public void turnRight() {
+        this.direction = (this.direction + 90) % 360;
+    }
+
 }
