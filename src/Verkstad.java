5024f38ba76292b3764cb55da69369d8502ae65f
Anajrim <115026399+Anajrim01@users.noreply.github.com>
2025-02-07 15:43:40 +0100

    fix: capacity checks and init array with size

--- src/Verkstad.java
+++ src/Verkstad.java
@@ -2,19 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Verkstad<T extends Car> {
-    private List<T> cars = new ArrayList<T>();
+    private List<T> cars;
     private int capacity;
 
     public Verkstad(int capacity) {
-        this.capacity = capacity;
         if (capacity < 0)
             throw new IllegalArgumentException("Capacity must be a positive integer");
+        this.capacity = capacity;
+        this.cars = new ArrayList<>(capacity); // init with capacity
     }
 
     public void addCar(T car) {
-        if (cars.size() >= capacity) {
+        if (cars.size() >= capacity)
             throw new FullCapacityException("There's no space currently. Max capacity: " + capacity + "reached.");
-        }
 
         cars.add(car);
     }
